4bd4726b0a9ad818f3a0b494c80642035b2fbfd3
w0x7ce <tian.r.king@gmail.com>
2022-02-05 03:14:10 -0900

    add stt

--- examples/ding-dong-bot.ts
+++ examples/ding-dong-bot.ts
@@ -16,7 +16,9 @@ import {
 import qrcodeTerminal from 'qrcode-terminal'
 import request from 'request'
 import fs from 'fs'
+import fetch from 'node-fetch'
 
+const url = 'http://0.0.0.0:61112'
 export enum MessageType {
   Unknown = 0,
   Attachment  = 1,    // Attach(6),
@@ -57,7 +59,7 @@ function onScan (qrcode: string, status: ScanStatus) {
 function onLogin (user: Contact) {
   log.info('StarterBot', '%s login', user)
 }
-const filePath:string = '/storage/lol/wechaty/getting-started/media/'
+const filePath:string = '/storage/lol/wechaty/wechat_telegram_ocr/media/'
 const t = 1
 async function onMessage (msg: Message) {
   log.info('StarterBot', msg.toString())
@@ -67,22 +69,50 @@ async function onMessage (msg: Message) {
     await fileBox.toFile(filePath + t + '.png', true)
     // await fileBox.toFile('/tmp/logo.jpg')
   }
+
+  if(msg.type() === MessageType.Audio){
+    const audioFileBox = await msg.toFileBox()
+    //const audioData: Buffer = await audioFileBox.toBuffer();
+    const audio_dir = filePath + t + '.silk' 
+    await audioFileBox.toFile(audio_dir, true)
+
+    const body = {
+      field1: '1',
+      lol: '2',
+      audio: "data:audio/silk;base64,"+fs.readFileSync(audio_dir, 'base64')
+      //img: "data:image/gif;base64,"+fs.readFileSync("/storage/lol/test/nodejs/octocat.png", 'base64')
+    };
+
+    const response = await fetch(url + '/api/audio/', {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {'Content-Type': 'application/json'}
+    });
+
+    const data = await response.json()
+    console.log(data)
+    await msg.say(JSON.stringify(data));
+    //await msg.say(data.toString())
+    // FileBox.toFile
+    //await fileBox.toFile('/storage/lol/wechaty/wechat_telegram_ocr/media/p.mp3', true)
+  }
   if (msg.text() === 'ding') {
     await msg.say('dong')
   }
+
   // 通过request发起post请求上传文件到服务器
   // 获取图片的base64
   // var imageData = fs.readFileSync(filePath1);
   // var imageBase64 = imageData.toString("base64");
-  const url = 'http://127.0.0.1:61111/pic/'
-  const data_fs =  fs.createReadStream('/storage/lol/wechaty/getting-started/media/1.png') // eslint-disable-line
-  const formData = {
-    file: data_fs,
-  }
-  request.post({ formData: formData, url:url }, (_error, _response, data) => {  // eslint-disable-line
-      msg.say(data) // eslint-disable-line
-    })             // eslint-disable-line
-  msg.say('Hi @Jade')// eslint-disable-line
+  // const url = 'http://127.0.0.1:61111/pic/'
+  // const data_fs =  fs.createReadStream('/storage/lol/wechaty/getting-started/media/1.png') // eslint-disable-line
+  // const formData = {
+  //   file: data_fs,
+  // }
+  // request.post({ formData: formData, url:url }, (_error, _response, data) => {  // eslint-disable-line
+  //     msg.say(data) // eslint-disable-line
+  //   })             // eslint-disable-line
+  // msg.say('Hi @Jade')// eslint-disable-line
 }
 
 const bot = WechatyBuilder.build({
