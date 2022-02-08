2920491ebc418a6a92d854fbef8ab01884eee940
w0x7ce <tian.r.king@gmail.com>
2022-02-08 06:58:57 -0900

    add  tts

--- examples/ding-dong-bot.ts
+++ examples/ding-dong-bot.ts
@@ -11,9 +11,9 @@ import {
   ScanStatus,
   WechatyBuilder,
   log,
-  
-}                  from 'wechaty'
-import { FileBox }      from 'file-box' // import { FileBox }      from 'file-box'
+
+} from 'wechaty'
+import { FileBox } from 'file-box' // import { FileBox }      from 'file-box'
 import qrcodeTerminal from 'qrcode-terminal'
 import request from 'request'
 import fs from 'fs'
@@ -22,29 +22,29 @@ import fetch from 'node-fetch'
 
 export enum MessageType {
   Unknown = 0,
-  Attachment  = 1,    // Attach(6),
-  Audio       = 2,    // Audio(1), Voice(34)
-  Contact     = 3,    // ShareCard(42)
+  Attachment = 1,    // Attach(6),
+  Audio = 2,    // Audio(1), Voice(34)
+  Contact = 3,    // ShareCard(42)
   ChatHistory = 4,    // ChatHistory(19)
-  Emoticon    = 5,    // Sticker: Emoticon(15), Emoticon(47)
-  Image       = 6,    // Img(2), Image(3)
-  Text        = 7,    // Text(1)
-  Location    = 8,    // Location(48)
+  Emoticon = 5,    // Sticker: Emoticon(15), Emoticon(47)
+  Image = 6,    // Img(2), Image(3)
+  Text = 7,    // Text(1)
+  Location = 8,    // Location(48)
   MiniProgram = 9,    // MiniProgram(33)
-  GroupNote   = 10,   // GroupNote(53)
-  Transfer    = 11,   // Transfers(2000)
+  GroupNote = 10,   // GroupNote(53)
+  Transfer = 11,   // Transfers(2000)
   RedEnvelope = 12,   // RedEnvelopes(2001)
-  Recalled    = 13,   // Recalled(10002)
-  Url         = 14,   // Url(5)
-  Video       = 15,   // Video(4), Video(43)
-  Post        = 16,   // Moment, Channel, Tweet, etc
+  Recalled = 13,   // Recalled(10002)
+  Url = 14,   // Url(5)
+  Video = 15,   // Video(4), Video(43)
+  Post = 16,   // Moment, Channel, Tweet, etc
 }
 
-function onLogout (user: Contact) {
+function onLogout(user: Contact) {
   log.info('StarterBot', '%s logout', user)
 }
 
-function onScan (qrcode: string, status: ScanStatus) {
+function onScan(qrcode: string, status: ScanStatus) {
   if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
     const qrcodeImageUrl = [
       'https://wechaty.js.org/qrcode/',
@@ -57,12 +57,12 @@ function onScan (qrcode: string, status: ScanStatus) {
     log.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status)
   }
 }
-function onLogin (user: Contact) {
+function onLogin(user: Contact) {
   log.info('StarterBot', '%s login', user)
 }
 
 let _flag = 0
-async function onMessage (msg: Message) {
+async function onMessage(msg: Message) {
   const url = 'http://0.0.0.0:61112'
   log.info('StarterBot', msg.toString())
   if (msg.type() == MessageType.Text) {
@@ -79,8 +79,8 @@ async function onMessage (msg: Message) {
       headers: { 'Content-Type': 'application/json' } // eslint-disable-line
     }); // eslint-disable-line
     const base64_data = await response.json()
-    const fileDataDecoded = Buffer.from(JSON.stringify(base64_data),'base64');
-    fs.writeFile("aa.wav", fileDataDecoded, function(err) {
+    const fileDataDecoded = Buffer.from(JSON.stringify(base64_data), 'base64');
+    fs.writeFile("aa.wav", fileDataDecoded, function (err) {
       console.log(err)
     });
     const wav_fileBox = FileBox.fromFile('aa.wav')
@@ -94,18 +94,18 @@ async function onMessage (msg: Message) {
     const audio_dir = imageBox.name
     await imageBox.toFile(imageBox.name, true)
     const body = {
-        lol: _flag,
-        pic_name: imageBox.name,
-        pic_data: "data:image/jpg;base64," + fs.readFileSync(audio_dir, 'base64')  // eslint-disable-line
+      lol: _flag,
+      pic_name: imageBox.name,
+      pic_data: "data:image/jpg;base64," + fs.readFileSync(audio_dir, 'base64')  // eslint-disable-line
     };  // eslint-disable-line
     const response = await fetch(url + '/api/style/', {
-        body: JSON.stringify(body),
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' } // eslint-disable-line
+      body: JSON.stringify(body),
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' } // eslint-disable-line
     }); // eslint-disable-line
     const data = await response.json()
     await msg.say(JSON.stringify(data)); // eslint-disable-line
-    _flag=0
+    _flag = 0
   }
 
   if (msg.type() === MessageType.Audio) {
@@ -113,15 +113,15 @@ async function onMessage (msg: Message) {
     const audio_dir = audioFileBox.name
     await audioFileBox.toFile(audioFileBox.name, true)
     const body = {
-        lol: '1',
-        audio_name: audioFileBox.name,
-        audio_data: "data:audio/silk;base64," + fs.readFileSync(audio_dir, 'base64')  // eslint-disable-line
-        // img: "data:image/gif;base64,"+fs.readFileSync("/storage/lol/test/nodejs/octocat.png", 'base64')
+      lol: '1',
+      audio_name: audioFileBox.name,
+      audio_data: "data:audio/silk;base64," + fs.readFileSync(audio_dir, 'base64')  // eslint-disable-line
+      // img: "data:image/gif;base64,"+fs.readFileSync("/storage/lol/test/nodejs/octocat.png", 'base64')
     };  // eslint-disable-line
     const response = await fetch(url + '/api/audio/', {
-        body: JSON.stringify(body),
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' } // eslint-disable-line
+      body: JSON.stringify(body),
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' } // eslint-disable-line
     }); // eslint-disable-line
     const data = await response.json()
     await msg.say(JSON.stringify(data)); // eslint-disable-line
@@ -147,9 +147,9 @@ async function onMessage (msg: Message) {
   }
   if (msg.text() === 'ding') {
     await msg.say('dong')
-    _flag=11
+    _flag = 11
   }
-  
+
 
   // 通过request发起post请求上传文件到服务器
   // 获取图片的base64
@@ -170,9 +170,9 @@ const bot = WechatyBuilder.build({
   name: 'ding-dong-bot',
 })
 
-bot.on('scan',    onScan)
-bot.on('login',   onLogin)
-bot.on('logout',  onLogout)
+bot.on('scan', onScan)
+bot.on('login', onLogin)
+bot.on('logout', onLogout)
 bot.on('message', onMessage)
 bot.start()
   .then(() => log.info('StarterBot', 'Starter Bot Started.'))
